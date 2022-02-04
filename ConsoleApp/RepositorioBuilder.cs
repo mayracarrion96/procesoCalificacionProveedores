01397af6d62e97eb4058ae5b4a575a2478c14df9
Mayra Carrión <61747592+mayracarrion96@users.noreply.github.com>
2022-02-03 22:10:38 -0500

    avance 03032022

--- ConsoleApp/RepositorioBuilder.cs
+++ ConsoleApp/RepositorioBuilder.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp
     public class RepositorioBuilder
     {
         const string DBTipo = "DBTipo";
-        enum DBTipoConn { SqlServer, Postgres, Memoria }
+        enum DBTipoConn { SqlServer, Postgres, Memoria, MySql }
         static Repositorio db;
 
         public static Repositorio Crear()
@@ -21,6 +21,7 @@ namespace ConsoleApp
             // Lee la configuración acerca de qué base usar del archivo App.config
             string dbtipo = ConfigurationManager.AppSettings[DBTipo];
             string conn = ConfigurationManager.ConnectionStrings[dbtipo].ConnectionString;
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
 
             // Construye la conección acorde con el tipo
             DbContextOptions<Repositorio> contextOptions;
@@ -34,6 +35,11 @@ namespace ConsoleApp
                 case nameof(DBTipoConn.Postgres):
                     contextOptions = new DbContextOptionsBuilder<Repositorio>()
                         .UseNpgsql(conn)
+                        .Options;
+                    break;
+                case nameof(DBTipoConn.MySql):
+                    contextOptions = new DbContextOptionsBuilder<Repositorio>()
+                        .UseMySql(conn, serverVersion)
                         .Options;
                     break;
                 default: // Por defecto usa la memoria como base de datos
